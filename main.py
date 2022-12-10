195ad95851d54603604e0537094a2090e3ddace1
N1lsE <nilseckerle2001@gmail.com>
2022-12-10 23:56:40 +0100

    getting the Logging to work

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import psutil
+import Utilitys
+
+l = Utilitys.Logger.getInstance()
 
 def CheckRunningGameExists():
 	for p in psutil.process_iter():
 		if  'loop hero' in str(p).lower():
 			return
-	print('No running game was found. [main.py CheckRunningGameExists()]')
+	l.Log('No running game was found. [main.py CheckRunningGameExists()]')
 	exit()
 
 def RunBot():
 	CheckRunningGameExists()
-	print('Starting the bot.')
+	l.Log('Starting the bot.')
 
 if __name__ == '__main__':
+
 	RunBot()
